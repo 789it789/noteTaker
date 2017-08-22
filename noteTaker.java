4916a40e0b6efc40becb491c9c964f2b71708993
Chase Alexander <789@gmx.it>
2017-08-22 00:20:17 -0500

    change main class

--- noteTaker.java
+++ noteTaker.java
@@ -41,25 +41,7 @@ public class noteTaker extends javax.swing.JFrame {
         );
 
         pack();
-    }                                                
- 
-    public static void main(String args[]) {
-        
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("GTK+".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(noteTaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        java.awt.EventQueue.invokeLater(() -> {
-            new noteTaker().setVisible(true);
-        });
-    }
+    }                                                    
                    
     private javax.swing.JScrollPane scrl;
     private javax.swing.JTextArea note;
